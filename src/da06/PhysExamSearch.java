8eebbe942628ea1cb73ae0be77af348a53d16519
solgitae <solgitae@gmail.com>
2022-07-01 21:29:36 +0900

    20220701

--- src/da06/PhysExamSearch.java
+++ src/da06/PhysExamSearch.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Scanner;
 
+//신체검사 데이터 배열에서 이진 검색하기
+
 public class PhysExamSearch {
 
     static class PhyscData {
@@ -47,10 +49,9 @@ public class PhysExamSearch {
                 new PhyscData("유지훈",168,0.4),
                 new PhyscData("김한결",169,0.8),
                 new PhyscData("홍준기",171,1.5),
-                new PhyscData("이수민",175,2.0),
-
                 new PhyscData("전서현",173,0.7),
-                new PhyscData("이호연",174,1.2)
+                new PhyscData("이호연",174,1.2),
+                new PhyscData("이수민",175,2.0)
         };
 
         System.out.print("몇 cm인 사람을 찾고 있나요? : ");
